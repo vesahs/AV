126dc8cd6f8f9c142e2fe1228754b864703ca4e0
vesahs <hasanajvesa@gmail.com>
2023-12-05 15:37:04 +0100

    added form validation

--- js/script.js
+++ js/script.js
@@ -15,3 +15,45 @@ document.addEventListener('DOMContentLoaded', function () {
         document.body.classList.remove('menu-open');
     });
 });
+
+let usernameRegex = /^[A-Za-z.-_][a-z]{2,8}$/;
+let emailRegex = /^[a-zA-Z.-_]+@+[a-z]+\.+[a-z]{2,3}$/;
+let passwordRegex = /^[a-zA-Z0-9]+$/;
+let phoneRegex = /^[0-9]{0,14}$/;
+
+function validateForm(){
+    let usernameInput = document.getElementById('username');
+    let usernameError = document.getElementById('usernameError');
+    let emailInput = document.getElementById('email');
+    let emailError = document.getElementById('emailError');
+    let passInput = document.getElementById('password');
+    let passError = document.getElementById('passError');
+    let phoneInput = document.getElementById('phone');
+    let phoneError = document.getElementById('phoneError');
+
+    usernameError.innerText = '';
+    emailError.innerText = '';
+    passError.innerText = '';
+    phoneError.innerText = '';
+
+    if(!usernameRegex.test(usernameInput.value)){
+        usernameError.innerText = 'Invalid username';
+        return false;
+    }
+
+    if(!emailRegex.test(emailInput.value)){
+        emailError.innerText = 'Invalid email';
+        return false;
+    }
+
+    if(!passwordRegex.test(passInput.value)){
+        passError.innerText = 'Invalid password';
+        return false;
+    }
+    if(!phoneRegex.test(phoneInput.value)){
+        phoneError.innerText = 'Invalid phone number';
+        return false;
+    }
+
+    alert('Name and email was corect!');
+}
